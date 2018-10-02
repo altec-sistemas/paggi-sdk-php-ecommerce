4e1d8dd01d67382c609d656e7144f4848dc8d351
Paulo Daniel <paulo.daniel@paggi.com>
2018-10-02 15:33:01 -0300

    Criacao da interface e da classe TokenValidation

---
+++ sdk/concretes/TokenValidation.php
@@ -0,0 +1,68 @@
+<?php
+/** 
+ * This file verify if the token is valid
+ *
+ * PHP version 5.4, 5.5, 5.6, 7.0, 7.1, 7.2
+ *
+ * @category Test_File
+ * @package  Paggi
+ * @author   Paggi Integracoes <email@email.com>
+ * @license  MIT www.www.www
+ * @link     http://url.com
+ */ 
+
+ namespace Paggi\SDK;
+ use Paggi\SDK\Interfaces;
+
+ /** 
+  * This file verify if the token is valid
+  *
+  * PHP version 5.4, 5.5, 5.6, 7.0, 7.1, 7.2
+  *
+  * @category Test_File
+  * @package  Paggi
+  * @author   Paggi Integracoes <email@email.com>
+  * @license  MIT www.www.www
+  * @link     http://url.com
+  */ 
+class TokenValidation
+{
+    /**
+     * Function responsible for the token's validation
+     *
+     * @param string $issuer       Token's issuer
+     * @param string $audience     Token's audience
+     * @param string $id           Token's id
+     * @param string $initialToken Authentication Token
+     * 
+     * @return boolean
+     */
+    public function isValidToken($issuer, $audience, $id, $initialToken)
+    {
+        
+    }
+
+    /**
+     * Function responsible for the token's expiration
+     *
+     * @param string $initialToken Authentication Token
+     * 
+     * @return boolean
+     */
+    public function isExpiredToken($initialToken)
+    {
+        
+    }
+
+    /**
+     * Function who verify if token's expiration date is within a month from today
+     *
+     * @param string $initialToken Authentication Token
+     * 
+     * @return boolean
+     */
+    public function isExpiringToken($initialToken)
+    {
+        
+    }
+}
---
+++ sdk/interfaces/ITokenValidation.php
@@ -0,0 +1,55 @@
+<?php
+/** 
+ * This file hold the Interface to control of authentication token
+ *
+ * PHP version 5.6, 7.0, 7.1, 7.2
+ *
+ * @category Test_File
+ * @package  Paggi
+ * @author   Paggi Integracoes <email@email.com>
+ * @license  MIT www.www.www
+ * @link     http://url.com
+ */ 
+namespace Paggi\SDK\Interfaces;
+
+/**
+ * This class holds functions for token validation
+ * 
+ * @category Test_Class
+ * @package  Paggi
+ * @author   Paggi Integracoes <email@email.com>
+ * @license  MIT www.www.www
+ * @link     http://url.com
+ */ 
+interface ITokenValidation
+{
+    /**
+     * Function who will validate the token
+     *
+     * @param [type] $issuer       Token issuer
+     * @param [type] $audience     Token audience
+     * @param [type] $id           Tokens id
+     * @param [type] $initialToken Token for authentication
+     * 
+     * @return boolean
+     */
+    public function isValidToken($issuer, $audience, $id, $initialToken);
+
+    /**
+     * Function who will verify if the token is expired
+     *
+     * @param [type] $initialToken Token for authentication
+     * 
+     * @return boolean
+     */
+    public function isExpiredToken($initialToken);
+
+    /**
+     * Function who verify if the token will expire within one month
+     *
+     * @param [type] $initialToken Token for authentication
+     * 
+     * @return boolean
+     */
+    public function isExpiringToken($initialToken);
+}
