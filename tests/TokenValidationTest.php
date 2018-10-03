ee4129f5eb6295857e24325de05d73077f91d53a
Paulo Daniel <paulo.daniel@paggi.com>
2018-10-03 13:48:03 -0300

    ALteração para segurança de token stage

--- tests/TokenValidationTest.php
+++ tests/TokenValidationTest.php
@@ -26,14 +26,7 @@ use Paggi\SDK;
 class TokenValidationTest extends \PHPUnit_Framework_TestCase
 {
     static private $_initialToken 
-        = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJQQUdHSSIsImV4"
-        . "cCI6NjIwMTY3Nzc0NjYsImlhdCI6MTUzNjc3NzQ2NiwiaXNzIjoiUEFHR0kiLCJqdG"
-         . "kiOiI2YjlmNDRiYy1hMGMwLTQ1ZDMtOTdmNC1kNmFlYmRiZjUwZGMiLCJuYmY"
-          . "iOjE1MzY3Nzc0NjUsInBlcm1pc3Npb25zIjpbeyJwYXJ0bmVyX2lkIjoiZjlmMjZiM"
-           . "2YtYWU0Ni00YzY5LTg5M2MtNTJiMTU3ZTkxMDkwIiwicGVybWlzc2lvbnMiOl"
-            . "sic3lzdGVtX3VzZXIiXX1dLCJzdWIiOiJkYzcxNDFkZC1hYjQzLTQ1NmMtOWM3Mi05"
-             . "MDNkOGFkYTAwZWUiLCJ0eXAiOiJhY2Nlc3MifQ.G6roX-MkbB7ofCkSOK5H8Z"
-              . "vnk5XIvDXp9gvr25gPbY0MF5-8E0wgutMsaows2cQcksUg8TgJqlaKTya9FsV9nA";
+        = envToken;
     /**
      * Function responsible to test the validation of "IsValidToken"
      *
