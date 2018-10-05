a675412382362c14162f6be92ff7175053f5e85a
Paulo Daniel <paulo.daniel@paggi.com>
2018-10-05 17:02:37 -0300

    Alteração para incluir itens no DI

--- ConfigDI.php
+++ ConfigDI.php
@@ -5,5 +5,11 @@ use Psr\Container\ContainerInterface;
 return [
     'TokenValidation' => function (ContainerInterface $c) {
         return new \Paggi\SDK\TokenValidation();
+    },
+    'EnvironmentConfiguration' => function (ContainerInterface $c) {
+        return new \Paggi\SDK\EnvironmentConfiguration();
+    },
+    'Inflector' => function (ContainerInterface $c) {
+        return new \Doctrine\Common\Inflector\Inflector();
     }
 ];
