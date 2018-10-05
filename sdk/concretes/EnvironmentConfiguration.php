b041d7b61e3dd398c91f82dd79170e006dba6427
Paulo Daniel <paulo.daniel@paggi.com>
2018-10-05 12:35:15 -0300

    Alteração de visibilidade psegundo PSR-2 e introdução de DI

--- sdk/concretes/EnvironmentConfiguration.php
+++ sdk/concretes/EnvironmentConfiguration.php
@@ -1,5 +1,5 @@
 <?php
-/** 
+/**
  * This file holds the basic configuration for the environment
  *
  * PHP version 5.6, 7.0, 7.1, 7.2
@@ -9,14 +9,16 @@
  * @author   Paggi Integracoes <ti-integracoes@paggi.com>
  * @license  GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
  * @link     http://developers.paggi.com
- */ 
+ */
 
  namespace Paggi\SDK;
+
  use Lcobucci\JWT\ValidationData;
  use Lcobucci\JWT\Parser;
  use Paggi\SDK\Interfaces\IEnvironmentConfiguration;
+ use DI;
 
- /** 
+ /**
   * This class verify the environment's configuration
   *
   * PHP version 5.6, 7.0, 7.1, 7.2
@@ -26,24 +28,35 @@
   * @author   Paggi Integracoes <ti-integracoes@paggi.com>
   * @license  GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
   * @link     http://developers.paggi.com
-  */ 
+  */
 class EnvironmentConfiguration implements IEnvironmentConfiguration
 {
-    private static $_token;
-    private static $_partnerId;
-    private static $_environment = true;
+    private static $token = null;
+    private static $partnerId = null;
+    private static $environment = true;
+    private static $container;
+
+    public function __construct()
+    {
+        $builder = new \DI\ContainerBuilder();
+        $builder->useAutowiring(false);
+        $builder->useAnnotations(false);
+        $builder->addDefinitions('ConfigDI.php');
+        self::$container = $builder->build();
+    }
 
     /**
      * Function who will set the token
      *
-     * @param string $token Token 
-     * 
+     * @param string $token Token
+     *
      * @return boolean
      */
     public function setToken($token)
     {
-        if (TokenValidation::isValidToken($token)) {
-            self::$_token = $token;
+        $tokenValidator = self::$container->get('TokenValidation');
+        if ($tokenValidator->isValidToken($token)) {
+            self::$token = $token;
             return true;
         }
         return false;
@@ -53,13 +66,13 @@ class EnvironmentConfiguration implements IEnvironmentConfiguration
      * Function who will set the environment
      *
      * @param boolean $environmentStatus true if staging
-     * 
+     *
      * @return boolean
      */
     public function setStaging($environmentStatus)
     {
         if (is_bool($environmentStatus)) {
-            self::$_environment = $environmentStatus;
+            self::$environment = $environmentStatus;
 
             return true;
         }
@@ -68,23 +81,23 @@ class EnvironmentConfiguration implements IEnvironmentConfiguration
 
     /**
      * Function who will set the partnerId using the token
-     * 
+     *
      * @param string $token token
-     * 
+     *
      * @return boolean
      */
-    public function setPartnerIdByToken($token) 
+    public function setPartnerIdByToken($token)
     {
-        $tokenObj 
+        $tokenObj
             = (!isset($token) || is_null($token) || empty($token)) ?
-                (new Parser())->parse((string) self::$_token) :
+                (new Parser())->parse((string) self::$token) :
                 (new Parser())->parse((string) $token);
         if (!$tokenObj->hasClaim('permissions')) {
             return false;
         }
         $partnerId = $tokenObj->getClaim('permissions')[0]->partner_id;
         if (isset($partnerId) && !is_null($partnerId) && !empty($partnerId)) {
-            self::$_partnerId = $partnerId;
+            self::$partnerId = $partnerId;
             return true;
         }
         return false;
@@ -92,15 +105,15 @@ class EnvironmentConfiguration implements IEnvironmentConfiguration
 
     /**
      * Function who will set the partnerId
-     * 
+     *
      * @param string $partnerId partnerId
-     * 
+     *
      * @return boolean
      */
-    public function setPartnerIdByPartnerId($partnerId) 
+    public function setPartnerIdByPartnerId($partnerId)
     {
         if (isset($partnerId) || !is_null($partnerId) || !empty($partnerId)) {
-            self::$_partnerId = $partnerId;
+            self::$partnerId = $partnerId;
             return true;
         }
         return false;
@@ -108,31 +121,31 @@ class EnvironmentConfiguration implements IEnvironmentConfiguration
 
     /**
      * Function who gets the token configured
-     * 
+     *
      * @return boolean
      */
     public function getToken()
     {
-        return self::$_token;
+        return self::$token;
     }
 
     /**
      * Function who verify what environment is configured
-     * 
+     *
      * @return boolean
      */
     public function isStaging()
     {
-        return self::$_environment;
+        return self::$environment;
     }
 
     /**
      * Function who gets the Partner's id
-     * 
+     *
      * @return boolean
      */
     public function getPartnerId()
     {
-        return self::$_partnerId;
+        return self::$partnerId;
     }
 }
--- sdk/interfaces/IEnvironmentConfiguration.php
+++ sdk/interfaces/IEnvironmentConfiguration.php
@@ -1,5 +1,5 @@
 <?php
-/** 
+/**
  * This file hold the Interface to control environment's configuration
  *
  * PHP version 5.6, 7.0, 7.1, 7.2
@@ -9,74 +9,74 @@
  * @author   Paggi Integracoes <ti-integracoes@paggi.com>
  * @license  GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
  * @link     http://developers.paggi.com
- */ 
+ */
 namespace Paggi\SDK\Interfaces;
 
 /**
- * This class holds functions for environment's configuration
- * 
+ * This class holds public functions for environment's configuration
+ *
  * @category Interface_Environment_Class
  * @package  Paggi
  * @author   Paggi Integracoes <ti-integracoes@paggi.com>
  * @license  GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
  * @link     http://developers.paggi.com
- */ 
+ */
 interface IEnvironmentConfiguration
 {
     /**
-     * Function who will set the token
+     * public function who will set the token
      *
-     * @param string $token Token 
-     * 
+     * @param string $token Token
+     *
      * @return boolean
      */
-    function setToken($token);
+    public function setToken($token);
 
     /**
-     * Function who will set the environment
+     * public function who will set the environment
      *
      * @param boolean $environmentStatus true if staging
-     * 
+     *
      * @return boolean
      */
-    function setStaging($environmentStatus);
+    public function setStaging($environmentStatus);
 
     /**
-     * Function who will set the setPartnerIdByToken
-     * 
+     * public function who will set the setPartnerIdByToken
+     *
      * @param string $token token
-     * 
+     *
      * @return boolean
      */
     public function setPartnerIdByToken($token);
-    
+
     /**
-     * Function who will set the setPartnerIdByPartnerId
-     * 
+     * public function who will set the setPartnerIdByPartnerId
+     *
      * @param string $partnerId partnerId
-     * 
+     *
      * @return boolean
      */
-    public function setPartnerIdByPartnerId($partnerId); 
+    public function setPartnerIdByPartnerId($partnerId);
 
     /**
-     * Function who gets the token configured
-     * 
+     * public function who gets the token configured
+     *
      * @return boolean
      */
-    function getToken();
+    public function getToken();
 
     /**
-     * Function who verify what environment is configured
-     * 
+     * public function who verify what environment is configured
+     *
      * @return boolean
      */
-    function isStaging();
+    public function isStaging();
 
     /**
-     * Function who gets the Partner's id
-     * 
+     * public function who gets the Partner's id
+     *
      * @return boolean
      */
-    function getPartnerId();
+    public function getPartnerId();
 }
