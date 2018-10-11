4d335d3230314597ddb083700c2057d42297cd7c
Paulo Daniel <paulo.daniel@paggi.com>
2018-10-11 18:49:37 -0300

    Correção dos comentários em Bank e Card

--- sdk/concretes/Card.php
+++ sdk/concretes/Card.php
@@ -1,10 +1,30 @@
 <?php
+/**
+ * This file controls all method related to cards
+ *
+ * PHP version 5.6, 7.0, 7.1, 7.2
+ *
+ * @category Card_File
+ * @package  Paggi
+ * @author   Paggi Integracoes <email@email.com>
+ * @license  GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @link     http://developers.paggi.com
+ */
 namespace Paggi\SDK;
 
 use Paggi\SDK\DynamicObjectGenerator;
 use Paggi\SDK\Traits\Create;
 use Paggi\SDK\Traits\Delete;
 
+ /**
+  * This file control Card's methods
+  *
+  * @category Card_Class
+  * @package  Paggi
+  * @author   Paggi Integracoes <ti-integracoes@paggi.com>
+  * @license  GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
+  * @link     http://developers.paggi.com
+  */
 class Card extends DynamicObjectGenerator
 {
     use Create, Delete;
